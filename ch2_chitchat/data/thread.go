5183a883004cba6486f120896c3daece062ec39c
ken5scal <kengoscal@gmail.com>
2018-06-26 08:47:00 +0900

    done

--- ch2_chitchat/data/thread.go
+++ ch2_chitchat/data/thread.go
@@ -11,3 +11,36 @@ type Thread struct {
 	UserId    int
 	CreatedAt time.Time
 }
+
+// extract all threads in the db
+func Threads() (threads []Thread, err error){
+	rows, err := Db.Query(
+		"SELECT id, uuid, topic ,user_id, created_at FROM threads ORDER BY created_at DESC")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		th := Thread{}
+		if err = rows.Scan(
+			&th.Id, &th.Uuid, &th.Topic, &th.UserId, &th.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, th)
+	}
+	rows.Close()
+	return
+}
+
+func (thread *Thread) NumReplies() (count int) {
+	rows, err := Db.Query("SELECT count(*) FROM posts where thread_id = $1")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		if err = rows.Scan(&count); err != nil {
+			return
+		}
+	}
+	rows.Close()
+	return
+}
