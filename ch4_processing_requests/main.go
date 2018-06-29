78b1fea701710f1a63b4660a100a7dc0883bd403
ken5scal <kengoscal@gmail.com>
2018-06-29 13:25:22 +0900

    ch4_request body"

--- ch4_processing_requests/main.go
+++ ch4_processing_requests/main.go
@@ -10,9 +10,22 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/body", body)
 	server.ListenAndServe()
 }
+
+func body(w http.ResponseWriter, r *http.Request) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	// you can call on the Read and Close methods of the Body field.
+	r.Body.Read(body)
+	fmt.Fprintln(w, string(body))
+}
+
 func headers(w http.ResponseWriter, r *http.Request) {
 	h := r.Header
+	fmt.Println(r.Header["Accept-Encoding"])
+	fmt.Println(r.Header.Get("Accept-Encoding"))
 	fmt.Fprintln(w, h)
 }
+
