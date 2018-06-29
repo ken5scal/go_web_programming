4e9fa485926023f8726ba121513e532afff18ee4
ken5scal <kengoscal@gmail.com>
2018-06-29 14:10:10 +0900

    Post FOrm

--- ch4_processing_requests/main.go
+++ ch4_processing_requests/main.go
@@ -14,9 +14,19 @@ func main() {
 	http.HandleFunc("/process", process)
 	server.ListenAndServe()
 }
+
 func process(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Fprintln(w, r.Form)
+	fmt.Fprint(w, "r.Form: ")
+	fmt.Fprintln(w,  r.Form)
+
+	// only get the value to the key `post`, form value is always prioritized
+	fmt.Fprint(w, "r.Form[\"post\": ")
+	fmt.Fprintln(w, r.Form["post"])
+
+	// just  get the form key-value pairs. Only supports application/x-www-form-urlencoded
+	fmt.Fprint(w, "r.PostForm: ")
+	fmt.Fprintln(w, r.PostForm)
 }
 
 // curl -id "first_name=sausheong&last_name=chang" 127.0.0.1:8080/body
