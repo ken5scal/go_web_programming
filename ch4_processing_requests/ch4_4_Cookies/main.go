be61a9d0833910ed1abb65bff4c545f1220164c1
ken5scal <kengoscal@gmail.com>
2018-06-30 16:27:46 +0900

    using cookies for flash messages

--- ch4_processing_requests/ch4_4_Cookies/main.go
+++ ch4_processing_requests/ch4_4_Cookies/main.go
@@ -4,14 +4,45 @@ import (
 	"net/http"
 	"time"
 	"fmt"
+	"encoding/base64"
 )
 
 func main() {
 	server := &http.Server{Addr: "127.0.0.1:8080"}
 	http.HandleFunc("/set_cookie", setCookie)
 	http.HandleFunc("/get_cookie", getCookie)
+	http.HandleFunc("/set_message", setMessage)
+	http.HandleFunc("/show_message", showMessage)
 	server.ListenAndServe()
 }
+
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintln(w, "No message found")
+		}
+	} else {
+		rc := http.Cookie{
+			Name: "flash",
+			MaxAge: -1,
+			Expires: time.Unix(1, 0 ),
+		}
+		http.SetCookie(w, &rc)
+		val, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(val))
+	}
+}
+
+func setMessage(w http.ResponseWriter, r *http.Request) {
+	msg := []byte("Hello World!")
+	c := http.Cookie{
+		Name: "flash",
+		Value: base64.URLEncoding.EncodeToString(msg),
+	}
+	http.SetCookie(w, &c)
+}
+
 func getCookie(w http.ResponseWriter, r *http.Request) {
 	h := r.Header["Cookie"]
 	fmt.Fprint(w, "All Cookies: ")
@@ -31,11 +62,15 @@ func getCookie(w http.ResponseWriter, r *http.Request) {
 func setCookie(w http.ResponseWriter, r *http.Request) {
 	c1 := http.Cookie{
 		Name: "first_cookie",
+		//  the cookie values need to be URL encoded in the header
+		// but, in this case, there is no special characters
 		Value: "Go Web Programming",
 		HttpOnly: true,
 	}
 	c2 := http.Cookie{
 		Name: "second_cookie",
+		//  the cookie values need to be URL encoded in the header
+		// but, in this case, there is no special characters
 		Value: "Manning Publications GO",
 		HttpOnly: true,
 	}
@@ -45,6 +80,7 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &c2)
 }
 
+// Cookie is defined in net/http package
 type Cookie struct {
 	Name string
 	Value string
