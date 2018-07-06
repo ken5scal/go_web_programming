ab729c311d92e617973d964954b2ebd0ac7125fb
ken5scal <kengoscal@gmail.com>
2018-07-07 02:09:13 +0900

    Include handleGet

---
+++ ch8_testing_your_application/ch8_4_test_dependency_injection/server.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"path"
+	"encoding/json"
+	"database/sql"
+	_ "github.com/lib/pq" // within the package, init function is kicked
+)
+
+type Post struct {
+	Db *sql.DB
+	Id int
+	Content string
+	Author string
+}
+
+var Db *sql.DB
+
+func main() {
+	var err error
+	db, err := sql.Open("postgres", "user=gwp dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/post/", handleRequest(&Post{Db: db}))
+	server.ListenAndServe()
+}
+
+func handleRequest(t Text) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		switch r.Method {
+		case "GET":
+			err = handleGet(w, r, t)
+		case "POST":
+			err = handlePost(w, r, t)
+		case "PUT":
+			err = handlePut(w, r, t)
+		case "DELETE":
+			err = handleDelete(w, r, t)
+		}
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	err = post.fetch(id)
+	if err != nil {
+		return
+	}
+	output, err := json.MarshalIndent(&post, "", "\t\t")
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	body := make([]byte, r.ContentLength)
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &post)
+	if err = post.Create(); err !=nil{
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	if err := post.fetch(id); err != nil {
+		return
+	}
+
+	body := make([]byte, r.ContentLength)
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &post)
+	if err = post.Update(); err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request, post Text) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+
+	if err := post.fetch(id); err != nil {
+		return
+	}
+
+	if err = post.Delete(); err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+}
+
+func (post *Post) fetch(id int) (err error) {
+	err = post.Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func (post *Post) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+func (post *Post) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
+
+// Text will know what to do and return the necessary data you want
+// This will be passed into handleRequest to inject sql.DB dependency
+type Text interface {
+	fetch(id int) (err error)
+	Create() (err error)
+	Update() (err error)
+	Delete() (err error)
+}
---
+++ ch8_testing_your_application/ch8_4_test_dependency_injection/server_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"encoding/json"
+	"strings"
+	"os"
+	"fmt"
+)
+
+var mux *http.ServeMux
+var writer *httptest.ResponseRecorder
+
+func TestMain(m *testing.M) {
+	setUp()           // runs only once for all test cases
+	code := m.Run()  //  The individual test case functions are called
+	os.Exit(code)
+}
+
+func setUp() {
+	mux = http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+	writer = httptest.NewRecorder()    //captures returne http response
+}
+
+func TestHandleGet(t *testing.T)  {
+	postId := 2
+	request, _:= http.NewRequest("GET", fmt.Sprintf("/post/%d", postId), nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	var post Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+	if post.Id != postId {
+		t.Errorf("Cannot retrieve JSON post")
+	}
+}
+
+func TestHandlePut(t *testing.T) {
+	postId := 2
+	json  := strings.NewReader(`{"content":"Updated post","author":"Kengo Suzuki"}`)
+	request, _ := http.NewRequest("PUT", fmt.Sprintf("/post/%d", postId), json)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+}
