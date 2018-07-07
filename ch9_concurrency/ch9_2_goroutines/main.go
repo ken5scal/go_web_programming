54d1d362c5deffd3c681968ed026bf4e0b546aae
ken5scal <kengoscal@gmail.com>
2018-07-07 13:10:38 +0900

    9.2.2: goroutine and performance

--- ch9_concurrency/ch9_2_goroutines/main.go
+++ ch9_concurrency/ch9_2_goroutines/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"time"
 )
 
@@ -10,13 +10,13 @@ func main() {
 
 func printNumbers1() {
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d ", i)
+	//	fmt.Printf("%d ", i)
 	}
 }
 
 func printLetters1() {
 	for i := 'A'; i < 'A' + 10; i ++ {
-		fmt.Printf("%c ", i)
+	//	fmt.Printf("%c ", i)
 	}
 }
 
@@ -32,20 +32,69 @@ func goPrint1() {
 }
 
 func printNumbers2() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%d ", i)
+		//fmt.Printf("%d ", i)
 	}
 }
 
 func printLetters2() {
-	for i := 'A'; i < 'A' + 10; i++ {
+	for i := 'A'; i < 'A' + 100; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%c ", i)
+		//fmt.Printf("%c ", i)
 	}
+}
+
+func print2() {
+	printNumbers2()
+	printLetters1()
 }
 
 func goPrint2() {
 	go printNumbers2()
 	go printLetters2()
 }
+
+//% go test -run x -bench . -cpu=1                                                       (git)-[master]
+//goos: darwin
+//goarch: amd64
+//pkg: github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines
+//BenchmarkPrint1         100000000               11.6 ns/op
+//BenchmarkGoPrint1        1000000              1058 ns/op
+//BenchmarkPrint2             2000            860882 ns/op
+//BenchmarkGoPrint2        1000000             14028 ns/op
+//PASS
+//ok      github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines 18.759s
+
+//% go test -run x -bench . -cpu=2                                                       (git)-[master]
+//goos: darwin
+//goarch: amd64
+//pkg: github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines
+//BenchmarkPrint1-2       100000000               11.5 ns/op
+//BenchmarkGoPrint1-2      5000000               397 ns/op
+//BenchmarkPrint2-2           2000            837545 ns/op
+//BenchmarkGoPrint2-2      1000000             13644 ns/op
+//PASS
+//ok      github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines 19.333s
+
+//% go test -run x -bench . -cpu=3                                                       (git)-[master]
+//goos: darwin
+//goarch: amd64
+//pkg: github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines
+//BenchmarkPrint1-3       100000000               11.8 ns/op
+//BenchmarkGoPrint1-3      5000000               304 ns/op
+//BenchmarkPrint2-3           2000            866585 ns/op
+//BenchmarkGoPrint2-3       300000             12320 ns/op
+//PASS
+//ok      github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines 8.717s
+
+//% go test -run x -bench .
+//goos: darwin
+//goarch: amd64
+//pkg: github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines
+//BenchmarkPrint1-4       100000000               11.4 ns/op
+//BenchmarkGoPrint1-4      5000000               307 ns/op
+//BenchmarkPrint2-4           2000            888034 ns/op
+//BenchmarkGoPrint2-4       200000              9971 ns/op
+//PASS
+//ok      github.com/ken5scal/go_web_programming/ch9_concurrency/ch9_2_goroutines 19.198s
--- ch9_concurrency/ch9_2_goroutines/main_test.go
+++ ch9_concurrency/ch9_2_goroutines/main_test.go
@@ -14,7 +14,32 @@ func TestGoPrint1(t *testing.T) {
 	time.Sleep(1 * time.Microsecond)
 }
 
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print1()
+	}
+}
+
+func BenchmarkGoPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint1()
+	}
+}
+
 func TestGoPrint2(t *testing.T) {
 	goPrint2()
 	time.Sleep(1 * time.Millisecond)
 }
+
+func BenchmarkPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print2()
+	}
+}
+
+func BenchmarkGoPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint2()
+	}
+}
+
