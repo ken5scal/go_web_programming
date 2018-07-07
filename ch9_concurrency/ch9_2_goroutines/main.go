aadaf1771f93c89611e6020d9cbd49caa4610b26
ken5scal <kengoscal@gmail.com>
2018-07-07 12:39:13 +0900

    9.2.1: using goroutines

--- ch9_concurrency/ch9_2_goroutines/main.go
+++ ch9_concurrency/ch9_2_goroutines/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 }
 
 func printNumbers1() {
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d", i)
+		fmt.Printf("%d ", i)
 	}
 }
 
@@ -23,6 +26,26 @@ func print1() {
 }
 
 func goPrint1() {
+	// Ends before goroutine can output to std
 	go printNumbers1()
 	go printLetters1()
+}
+
+func printNumbers2() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%d ", i)
+	}
+}
+
+func printLetters2() {
+	for i := 'A'; i < 'A' + 10; i++ {
+		time.Sleep(1 * time.Microsecond)
+		fmt.Printf("%c ", i)
+	}
+}
+
+func goPrint2() {
+	go printNumbers2()
+	go printLetters2()
 }
