4ba98ecf479cab684a6031d2ff00ee03df28d328
ken5scal <kengoscal@gmail.com>
2018-07-10 08:00:14 +0900

    9.4.2: The photo mosaic web application

--- ch9_concurrency/ch9_4_concurrency_for_web_app/main.go
+++ ch9_concurrency/ch9_4_concurrency_for_web_app/main.go
@@ -7,11 +7,97 @@ import (
 	"io/ioutil"
 	"os"
 	"math"
+	"net/http"
+	"html/template"
+	"time"
+	"strconv"
+	"image/draw"
+	"bytes"
+	"image/jpeg"
+	"encoding/base64"
 )
 
 var TILESDB map[string][3]float64
 
 func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", upload)
+	mux.HandleFunc("/mosaic", mosaic)
+	server := &http.Server{
+		Addr : "127.0.0.1:8080",
+		Handler: mux,
+	}
+
+	TILESDB = titleDB()
+	server.ListenAndServe()
+}
+func upload(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("upload.html")
+	t.Execute(w, nil)
+}
+
+func mosaic(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
+
+	r.ParseMultipartForm(10485760)
+	file, _, _ := r.FormFile("image")
+	defer file.Close()
+	tileSize, _ := strconv.Atoi(r.FormValue("tile_size"))
+
+	original, _, _ := image.Decode(file)
+	bounds := original.Bounds()
+
+	newImage := image.NewNRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
+
+	// every time image uploaded, clone the db
+	db := cloneTilesDB()
+
+	// we take the top-left pixel and assume that's the average color for each tile
+	sp := image.Point{0, 0}
+
+	// Iterates through target image
+	for y:= bounds.Min.Y; y < bounds.Max.Y; y = y + tileSize {
+		for x := bounds.Min.X; x < bounds.Max.X; x = x + tileSize {
+			r, g, b, _ := original.At(x, y).RGBA()
+			color := [3]float64{float64(r), float64(g), float64(b)}
+
+			nearest := nearest(color, &db)
+			file, err := os.Open(nearest)
+			if err == nil {
+				img, _, err := image.Decode(file)
+				if err == nil {
+					t := resize(img, tileSize)
+					tile := t.SubImage(t.Bounds())
+					tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+					draw.Draw(newImage, tileBounds, tile, sp, draw.Src)
+				} else {
+					fmt.Println("error: ", err, nearest)
+				}
+			} else {
+				fmt.Println("error: ", nearest)
+			}
+			file.Close()
+		}
+	}
+
+	// Encode in JPEG, deliver to browser in base64 string
+	buf1 := new(bytes.Buffer)
+	jpeg.Encode(buf1, original, nil)
+	originalStr := base64.StdEncoding.EncodeToString(buf1.Bytes())
+
+	buf2 := new(bytes.Buffer)
+	jpeg.Encode(buf2, newImage, nil)
+	mosaic := base64.StdEncoding.EncodeToString(buf2.Bytes())
+	t1 := time.Now()
+	images := map[string]string{
+		"original": originalStr,
+		"mosaic": mosaic,
+		"duration": fmt.Sprintf("%v ", t1.Sub(t0)),
+	}
+	t, _ := template.ParseFiles("results.html")
+	t.Execute(w, images)
 }
 
 func averageColor(img image.Image) [3]float64 {
@@ -54,7 +140,6 @@ func titleDB() map[string][3]float64 {
 		name := "tiles/" + f.Name()
 		file, err := os.Open(name)
 		if err == nil {
-			defer file.Close()
 			img, _, err := image.Decode(file)
 			if err == nil {
 				db[name] = averageColor(img)
@@ -64,6 +149,7 @@ func titleDB() map[string][3]float64 {
 		} else {
 			fmt.Println("cannot open file", name ,err)
 		}
+		file.Close()
 	}
 	fmt.Println("Finished populating tiles db.")
 	return db
@@ -91,6 +177,7 @@ func sq(n float64) float64 {
 	return n * n
 }
 
+// clone the tile db each tiem the photo mosaic is generated
 func cloneTilesDB() map[string][3]float64 {
 	db := make(map[string][3]float64)
 	for k, v := range TILESDB {
