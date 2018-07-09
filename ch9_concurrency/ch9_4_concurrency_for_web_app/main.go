4e722492bd07c74a341c2a4095a627ddcf7cedd0
ken5scal <kengoscal@gmail.com>
2018-07-10 07:28:51 +0900

    9.4.1: crating the photo mosaic

--- ch9_concurrency/ch9_4_concurrency_for_web_app/main.go
+++ ch9_concurrency/ch9_4_concurrency_for_web_app/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"image"
 	"image/color"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"math"
 )
+
+var TILESDB map[string][3]float64
 
 func main() {
 }
@@ -29,11 +35,66 @@ func resize(in image.Image, newWidth int) image.NRGBA {
 		bounds.Min.Y/ratio,
 		bounds.Max.X/ratio,
 		bounds.Max.Y/ratio))
-	for y, j := bounds.Min.Y, bounds.Min.Y; y < bounds.Max.Y; y, j = y+ratio, j+1{
-		for x, i := bounds.Min.X, bounds.Min.X; x < bounds.Max.X; x, i = x+ratio, i+1{
+	for y, new_y := bounds.Min.Y, bounds.Min.Y; y < bounds.Max.Y; y, new_y = y+ratio, new_y+1{
+		for x, new_x := bounds.Min.X, bounds.Min.X; x < bounds.Max.X; x, new_x = x+ratio, new_x+1{
 			r, g, b, a := in.At(x, y).RGBA()
-			out.SetNRGBA(i, j, color.NRGBA{uint8(r>>8), uint8(g>>8), uint8(b>>8), uint8(a>>8)})
+			out.SetNRGBA(new_x, new_y, color.NRGBA{uint8(r>>8), uint8(g>>8), uint8(b>>8), uint8(a>>8)})
 		}
 	}
 	return *out
+}
+
+// creates a db of the tile picture by scanning the dir
+// it will store average color.
+func titleDB() map[string][3]float64 {
+	fmt.Println("Start populating titles db ...")
+	db := make(map[string][3]float64)
+	files, _ := ioutil.ReadDir("titles")
+	for _, f := range files {
+		name := "tiles/" + f.Name()
+		file, err := os.Open(name)
+		if err == nil {
+			defer file.Close()
+			img, _, err := image.Decode(file)
+			if err == nil {
+				db[name] = averageColor(img)
+			} else {
+				fmt.Println("error in populating TILEDB:", err, name)
+			}
+		} else {
+			fmt.Println("cannot open file", name ,err)
+		}
+	}
+	fmt.Println("Finished populating tiles db.")
+	return db
+}
+
+// finding the nearest matching image
+func nearest(target [3]float64, db *map[string][3]float64) string {
+	var filename string
+	smallest := 1000000.0
+	for k, v := range *db {
+		dist := distance(target, v)
+		if dist < smallest {
+			filename, smallest = k, dist
+		}
+	}
+	delete(*db, filename)
+	return filename
+}
+
+func distance(p1 [3]float64, p2 [3]float64) float64 {
+	return math.Sqrt(sq(p2[0]-p1[0]) + sq(p2[1]-p1[1])+sq(p2[2]-p1[2]))
+}
+
+func sq(n float64) float64 {
+	return n * n
+}
+
+func cloneTilesDB() map[string][3]float64 {
+	db := make(map[string][3]float64)
+	for k, v := range TILESDB {
+		db[k] =v
+	}
+	return db
 }
