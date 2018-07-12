b976ed2cf951478a948a88207721648f43471118
ken5scal <kengoscal@gmail.com>
2018-07-12 23:58:46 +0900

    debugging"

--- ch9_concurrency/ch9_4_concurrency_for_web_app/concurrent_mosaic/main.go
+++ ch9_concurrency/ch9_4_concurrency_for_web_app/concurrent_mosaic/main.go
@@ -16,17 +16,20 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting mosaic server ...")
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
 	mux.HandleFunc("/", upload)
 	mux.HandleFunc("/mosaic", mosaic)
+
 	server := &http.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: mux,
 	}
-	// building up the source tile database
 	TILESDB = tilesDB()
+
 	fmt.Println("Mosaic server started.")
 	server.ListenAndServe()
 }
@@ -43,28 +46,27 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 	file, _, _ := r.FormFile("image")
 	defer file.Close()
 	tileSize, _ := strconv.Atoi(r.FormValue("tile_size"))
-	// decode and get original image
+	//
+	//   // decode and get original image
 	original, _, _ := image.Decode(file)
 	bounds := original.Bounds()
-	// build up the tiles database
 	db := cloneTilesDB()
-
-	// Fanning out, cutting up image for independent processing
+	// fan-out
 	c1 := cut(original, &db, tileSize, bounds.Min.X, bounds.Min.Y, bounds.Max.X/2, bounds.Max.Y/2)
 	c2 := cut(original, &db, tileSize, bounds.Max.X/2, bounds.Min.Y, bounds.Max.X, bounds.Max.Y/2)
 	c3 := cut(original, &db, tileSize, bounds.Min.X, bounds.Max.Y/2, bounds.Max.X/2, bounds.Max.Y)
 	c4 := cut(original, &db, tileSize, bounds.Max.X/2, bounds.Max.Y/2, bounds.Max.X, bounds.Max.Y)
+	// fan-in
 	c := combine(bounds, c1, c2, c3, c4)
-
 	buf1 := new(bytes.Buffer)
 	jpeg.Encode(buf1, original, nil)
 	originalStr := base64.StdEncoding.EncodeToString(buf1.Bytes())
 
 	t1 := time.Now()
-	images := map[string]string {
+	images := map[string]string{
 		"original": originalStr,
-		"mosaic": <- c,
-		"duration": fmt.Sprint("%v ", t1.Sub(t0)),
+		"mosaic":   <-c,
+		"duration": fmt.Sprintf("%v ", t1.Sub(t0)),
 	}
 
 	t, _ := template.ParseFiles("results.html")
