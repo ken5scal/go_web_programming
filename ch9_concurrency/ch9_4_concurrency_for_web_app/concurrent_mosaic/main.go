56c482f20061aca9616f1777d8f27eb1a1870ed0
ken5scal <kengoscal@gmail.com>
2018-07-13 00:50:50 +0900

    update

--- ch9_concurrency/ch9_4_concurrency_for_web_app/concurrent_mosaic/main.go
+++ ch9_concurrency/ch9_4_concurrency_for_web_app/concurrent_mosaic/main.go
@@ -32,6 +32,7 @@ func main() {
 
 	fmt.Println("Mosaic server started.")
 	server.ListenAndServe()
+
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +40,80 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+// cut out the image and return individual channels with image.Image
+// no encoding of JPEG
+func cut(original image.Image, db *DB, tileSize, x1, y1, x2, y2 int) <-chan image.Image {
+	c := make(chan image.Image)
+	sp := image.Point{0, 0}
+	go func() {
+		newimage := image.NewNRGBA(image.Rect(x1, y1, x2, y2))
+		for y := y1; y < y2; y = y + tileSize {
+			for x := x1; x < x2; x = x + tileSize {
+				r, g, b, _ := original.At(x, y).RGBA()
+				color := [3]float64{float64(r), float64(g), float64(b)}
+				nearest := db.nearest(color)
+				file, err := os.Open(nearest)
+				if err == nil {
+					img, _, err := image.Decode(file)
+					if err == nil {
+						t := resize(img, tileSize)
+						tile := t.SubImage(t.Bounds())
+						tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+						draw.Draw(newimage, tileBounds, tile, sp, draw.Src)
+					} else {
+						fmt.Println("error in decoding nearest", err, nearest)
+					}
+				} else {
+					fmt.Println("error opening file when creating mosaic:", nearest)
+				}
+				file.Close()
+			}
+		}
+		c <- newimage.SubImage(newimage.Rect)
+	}()
+
+	return c
+}
+
+// combine the images and return the encoding string
+func combine(r image.Rectangle, c1, c2, c3, c4 <-chan image.Image) <-chan string {
+	c := make(chan string)
+	// start a goroutine
+	go func() {
+		var wg sync.WaitGroup
+		newimage := image.NewNRGBA(r)
+		copy := func(dst draw.Image, r image.Rectangle, src image.Image, sp image.Point) {
+			draw.Draw(dst, r, src, sp, draw.Src)
+			wg.Done()
+		}
+		wg.Add(4)
+		var s1, s2, s3, s4 image.Image
+		var ok1, ok2, ok3, ok4 bool
+		for {
+			select {
+			case s1, ok1 = <-c1:
+				go copy(newimage, s1.Bounds(), s1, image.Point{r.Min.X, r.Min.Y})
+			case s2, ok2 = <-c2:
+				go copy(newimage, s2.Bounds(), s2, image.Point{r.Max.X / 2, r.Min.Y})
+			case s3, ok3 = <-c3:
+				go copy(newimage, s3.Bounds(), s3, image.Point{r.Min.X, r.Max.Y / 2})
+			case s4, ok4 = <-c4:
+				go copy(newimage, s4.Bounds(), s4, image.Point{r.Max.X / 2, r.Max.Y / 2})
+			}
+			if ok1 && ok2 && ok3 && ok4 {
+				break
+			}
+		}
+		// wait till all copy goroutines are complete
+		wg.Wait()
+		buf2 := new(bytes.Buffer)
+		jpeg.Encode(buf2, newimage, nil)
+		c <- base64.StdEncoding.EncodeToString(buf2.Bytes())
+	}()
+	return c
+}
+
+//  Handler function for fan-out and fan-in
 func mosaic(w http.ResponseWriter, r *http.Request) {
 	t0 := time.Now()
 	// get the content from the POSTed form
@@ -71,87 +146,4 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("results.html")
 	t.Execute(w, images)
-}
-
-func cut(original image.Image, db *DB, tileSize, x1, y1, x2, y2 int) <-chan image.Image {
-	c := make(chan image.Image)
-	sp := image.Point{0, 0}
-	go func() {
-		// create a new image for the mosaic (IN CASE OF Processing entire image at once)
-		newimage := image.NewNRGBA(image.Rect(x1, y1, x2, y2))
-		for y := y1; y < y2; y = y + tileSize {
-			for x := x1; x < x2; x = x + tileSize {
-				// use the top left most pixel as the average color
-				r, g, b, _ := original.At(x, y).RGBA()
-				color := [3]float64{float64(r), float64(g), float64(b)}
-				// get the closest tile from the tiles DB
-				nearest := db.nearest(color)
-				file, err := os.Open(nearest)
-				if err == nil {
-					img, _, err := image.Decode(file)
-					if err == nil {
-						// resize the tile to the correct size
-						t := resize(img, tileSize)
-						tile := t.SubImage(t.Bounds())
-						tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
-						// draw the tile into the mosaic
-						draw.Draw(newimage, tileBounds, tile, sp, draw.Src)
-					} else {
-						fmt.Println("error:", err, nearest)
-					}
-				} else {
-					fmt.Println("error:", nearest)
-				}
-				file.Close()
-			}
-		}
-		c <- newimage.SubImage(newimage.Rect)
-	}()
-
-	return c
-}
-
-// returning a receive-only channel
-func combine(r image.Rectangle, c1, c2, c3, c4 <-chan image.Image) <-chan string {
-	c := make(chan string)
-
-	go func() {
-		// wait until all subimages copied to final image
-		var wg sync.WaitGroup
-		img := image.NewNRGBA(r)
-
-		copy := func(dst draw.Image, r image.Rectangle, src image.Image, sp image.Point) {
-			draw.Draw(dst, r, src, sp, draw.Src)
-			// decrements counter as subimages copied over
-			wg.Done()
-		}
-
-		wg.Add(4)
-		var s1, s2, s3, s4 image.Image
-		var ok1, ok2, ok3, ok4 bool
-
-		for {
-			// start processing whichever segment that comes first
-			select {
-			case s1, ok1 = <-c1:
-				go copy(img, s1.Bounds(), s1, image.Point{r.Min.X, r.Min.Y})
-			case s2, ok2 = <-c2:
-				go copy(img, s2.Bounds(), s2, image.Point{r.Max.X / 2, r.Min.Y})
-			case s3, ok3 = <-c3:
-				go copy(img, s3.Bounds(), s3, image.Point{r.Min.X, r.Max.Y / 2})
-			case s4, ok4 = <-c4:
-				go copy(img, s4.Bounds(), s4, image.Point{r.Max.X / 2, r.Max.Y / 2})
-			}
-			if ok1 && ok2 && ok3 && ok4 {
-				break
-			}
-		}
-
-		wg.Wait()
-		buf2 := new(bytes.Buffer)
-		jpeg.Encode(buf2, img, nil)
-		c <- base64.StdEncoding.EncodeToString(buf2.Bytes())
-	}()
-
-	return c
 }
